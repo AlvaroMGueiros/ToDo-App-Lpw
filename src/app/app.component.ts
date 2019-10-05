f0552a59cbd0b90ae004d14837a6321f7b878185
alvaromgueiros <alvaromgueiros@gmail.com>
2019-10-04 22:24:17 -0300

    App todo com os 5 problemas feitos

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from "@angular/core";
+import { Model, TodoItem } from "./model";
+@Component({
+  selector: "todo-app",
+  templateUrl: "app.component.html",
+  
+})
+export class AppComponent {
+  model = new Model();
+  getName(){
+    return this.model.user;
+  }
+  getTodoItems(){
+    return this.model.items.filter(item => !item.done);
+  }
+  addItem(newItem, priority, data){
+    if(newItem != ""){
+      this.model.items.push(new TodoItem(newItem, false, priority, data));
+    }
+    this.model.items.sort((a: TodoItem, b: TodoItem) => {
+       return +new Date(a.data) - +new Date(b.data);
+    })
+  }
+getAltapriority(){
+    if(this.model.items.priority.equals("Alta")){
+      return this.model.items.priority;
+    }
+  }
+  getTodoItemsconcluidos(){
+    return this.model.items.filter(item => item.done);
+    }
+  
+}
+ 
